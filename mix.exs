93668e285688e036c6f2cce881e69d485d56f715
Selamawit <ezanawittadele@gmail.com>
2025-05-09 18:16:42 +0300

    feat:test

--- mix.exs
+++ mix.exs
@@ -9,7 +9,12 @@ defmodule Project.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      releases: [
+        project: [
+          include_executables_for: [:unix]
+        ]
+      ]
     ]
   end
 
