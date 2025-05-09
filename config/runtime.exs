f47da8842f1da0a40fe5497e73313f30978f835b
Selamawit <ezanawittadele@gmail.com>
2025-05-09 18:18:34 +0300

    push

--- config/runtime.exs
+++ config/runtime.exs
@@ -39,16 +39,12 @@ if config_env() == :prod do
   config :project, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :project, ProjectWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME"), port: 443],
     http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
+      port: String.to_integer(System.get_env("PORT") || "4000"),
+      transport_options: [socket_opts: [:inet6]]
     ],
-    secret_key_base: secret_key_base
+    secret_key_base: System.get_env("SECRET_KEY_BASE")
 
   # ## SSL Support
   #
